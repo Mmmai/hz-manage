b3984dc9533905f046ea84136c04317fec04e5fa
maish <787121533@qq.com>
2025-01-10 10:41:34 +0800

    提交dist编译后的ui代码

---
+++ hz-ui/dist/assets/config-DlYwUTXT.js
@@ -0,0 +1,1 @@
+import{ce as a,r as o}from"./index-CRk9gF9P.js";const m=a("configs",()=>{const t=o({}),l=o(!1),e=o(0*1e3);return{gmCry:t,setGmCry:s=>{t.value=s},showAllPass:l,updateShowAllPass:s=>{l.value=s},setShowAllPassTime:s=>{e.value=s},showAllPassTime:e,updateShowAllPassTime:s=>{e.value=s}}},{persist:!0});export{m as u};
